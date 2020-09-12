2af25d13d3a6b690570bea561cf4c3adc6055858
Tony Liu <mrtoniliu@gmail.com>
2020-09-12 20:15:37 +0100

    Initial Commit

---
+++ Shared/mySetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySetGameApp.swift
+//  Shared
+//
+//  Created by ShinyMimikyu on 12/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct mySetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
